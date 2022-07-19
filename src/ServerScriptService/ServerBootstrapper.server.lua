ad61cf713fc5c1f5cd1d9847e78081fba2bcd807
bowlimon <bowlimon@protonmail.com>
2022-07-19 17:18:41 +0100

    Refactor to allow for proper development with repo as submodule

--- src/ServerScriptService/ServerBootstrapper.server.lua
+++ src/ServerScriptService/ServerBootstrapper.server.lua
@@ -1,6 +1,22 @@
 
+for _, folder in next, game:GetService("ServerScriptService"):GetChildren() do
+    if folder:IsA("Folder") then
+        destFolder = nil
+
+        if folder.Name == "Client" then
+            destFolder = game:GetService("StarterPlayer"):WaitForChild("StarterPlayerScripts"):WaitForChild("ClientSource")
+        elseif folder.Name == "Server" then
+            destFolder = game:GetService("ServerStorage"):WaitForChild("ServerSource")
+        elseif folder.Name == "SharedModules" then
+            destFolder = game:GetService("ReplicatedStorage"):WaitForChild("SharedModules")
+        end
+
+        for _, child in next, folder:GetChildren() do
+            folder:Clone().Parent = child
+        end
+    end
+end
+
 require(game:GetService("ReplicatedStorage"):WaitForChild("Bootstrapper"))()
 
 shared.require("/modules/Network")
-
-shared.require("/src/Test/TalkToClient")
